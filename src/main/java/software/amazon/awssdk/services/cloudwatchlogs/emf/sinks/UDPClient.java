0a01f0dd298f37f0499fc102592d339f41914a23
Yao Zhao <yaozhaoy@gmail.com>
2020-07-29 12:20:09 -0700

    Fixed comments

--- src/main/java/software/amazon/awssdk/services/cloudwatchlogs/emf/sinks/UDPClient.java
+++ src/main/java/software/amazon/awssdk/services/cloudwatchlogs/emf/sinks/UDPClient.java
@@ -14,18 +14,11 @@ class UDPClient implements SocketClient {
 
     private InetSocketAddress inetAddress;
     private DatagramSocket datagramSocket;
-    private int port;
+    private Endpoint endpoint;
 
     UDPClient(Endpoint endpoint) {
         inetAddress = new InetSocketAddress(endpoint.getHost(), endpoint.getPort());
-        port = endpoint.getPort();
-
-        try {
-            datagramSocket = new DatagramSocket();
-        } catch (SocketException ex) {
-            final String msg = "Could not instantiate DatagramSocket to " + endpoint.getHost();
-            log.error(msg, ex);
-        }
+        this.endpoint = endpoint;
     }
 
 
@@ -38,8 +31,13 @@ class UDPClient implements SocketClient {
 
     private synchronized void flush(DatagramPacket packet) {
         try {
+            if (datagramSocket == null) {
+                createSocket();
+            }
             if (datagramSocket != null) {
                 datagramSocket.send(packet);
+            } else {
+                log.warn("No DatagramSocket available. Message would be dropped");
             }
         } catch (IOException ex) {
             final String msg = "Failed to send DatagramPacket to " + inetAddress;
@@ -51,6 +49,15 @@ class UDPClient implements SocketClient {
     public void close() throws IOException {
         if (datagramSocket != null) {
             datagramSocket.close();
+        }
+    }
+
+    private void createSocket() {
+        try {
+            datagramSocket = new DatagramSocket();
+        } catch (SocketException ex) {
+            final String msg = "Could not instantiate DatagramSocket to " + endpoint.getHost();
+            log.error(msg, ex);
         }
     }
 }
