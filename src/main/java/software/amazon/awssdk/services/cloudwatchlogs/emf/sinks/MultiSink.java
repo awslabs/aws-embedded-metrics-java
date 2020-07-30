8163f349c89e4f3eb31b6022b79bac03d1384e2f
yaozhaoy <66138407+yaozhaoy@users.noreply.github.com>
2020-07-30 15:48:57 -0700

    Remove some redandant code (#12)

--- src/main/java/software/amazon/awssdk/services/cloudwatchlogs/emf/sinks/MultiSink.java
+++ src/main/java/software/amazon/awssdk/services/cloudwatchlogs/emf/sinks/MultiSink.java
@@ -3,8 +3,6 @@ package software.amazon.awssdk.services.cloudwatchlogs.emf.sinks;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Singular;
-import software.amazon.awssdk.services.cloudwatchlogs.emf.logger.FlushException;
-import software.amazon.awssdk.services.cloudwatchlogs.emf.model.EMFLogItem;
 import software.amazon.awssdk.services.cloudwatchlogs.emf.model.MetricsContext;
 
 import java.util.List;
@@ -19,13 +17,6 @@ public class MultiSink implements ISink {
     @Singular
     @NonNull
     private List<ISink> sinks;
-
-    @Override
-    public void accept(List<EMFLogItem> logItems) throws FlushException {
-        for (ISink sink : sinks) {
-            sink.accept(logItems);
-        }
-    }
 
     @Override
     public void accept(MetricsContext context) {
--- src/test/java/software/amazon/awssdk/services/cloudwatchlogs/emf/sinks/SinkShunt.java
+++ src/test/java/software/amazon/awssdk/services/cloudwatchlogs/emf/sinks/SinkShunt.java
@@ -1,19 +1,11 @@
 package software.amazon.awssdk.services.cloudwatchlogs.emf.sinks;
 
-import software.amazon.awssdk.services.cloudwatchlogs.emf.logger.FlushException;
-import software.amazon.awssdk.services.cloudwatchlogs.emf.model.EMFLogItem;
 import software.amazon.awssdk.services.cloudwatchlogs.emf.model.MetricsContext;
 
-import java.util.List;
 
 public class SinkShunt implements ISink {
 
     private MetricsContext context;
-
-    @Override
-    public void accept(List<EMFLogItem> logItems) throws FlushException {
-
-    }
 
     @Override
     public void accept(MetricsContext context) {
