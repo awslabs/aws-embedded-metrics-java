e794add30b918554334ae2542e5c4a2e81c7f019
Yao Zhao <yaozhaoy@gmail.com>
2020-08-06 17:33:27 -0700

    Add a no-arg constructor for MetricsLogger

--- src/main/java/software/amazon/awssdk/services/cloudwatchlogs/emf/logger/MetricsLogger.java
+++ src/main/java/software/amazon/awssdk/services/cloudwatchlogs/emf/logger/MetricsLogger.java
@@ -15,6 +15,10 @@ public class MetricsLogger {
     private MetricsContext context;
     private Environment environment;
 
+    public MetricsLogger() {
+        this(new EnvironmentProvider());
+    }
+
     public MetricsLogger(EnvironmentProvider environmentProvider) {
         this(environmentProvider, new MetricsContext());
     }
