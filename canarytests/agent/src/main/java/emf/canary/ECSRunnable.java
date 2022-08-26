c6089a10919d2a905645028094d595d01d0ee4f9
Mark Kuhn <kuhnmar@amazon.com>
2022-08-26 15:11:17 -0700

    explicitly set ecs env

--- canarytests/agent/src/main/java/emf/canary/ECSRunnable.java
+++ canarytests/agent/src/main/java/emf/canary/ECSRunnable.java
@@ -2,6 +2,7 @@ package emf.canary;
 
 import software.amazon.cloudwatchlogs.emf.config.Configuration;
 import software.amazon.cloudwatchlogs.emf.config.EnvironmentConfigurationProvider;
+import software.amazon.cloudwatchlogs.emf.environment.Environments;
 import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
 import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
 import software.amazon.cloudwatchlogs.emf.model.Unit;
@@ -13,14 +14,17 @@ public class ECSRunnable implements Runnable {
 
     @Override
     public void run() {
+        final Configuration config = EnvironmentConfigurationProvider.getConfig();
+        config.setEnvironmentOverride(Environments.ECS);
+        config.setLogGroupName("/Canary/Java/CloudWatchAgent/Metrics");
+
         MetricsLogger logger = new MetricsLogger();
 
         String version = logger.getClass().getPackage().getImplementationVersion();
         if (version == null) {
             version = "Unknown";
         }
-        Configuration config = EnvironmentConfigurationProvider.getConfig();
-        config.setLogGroupName("/Canary/Java/CloudWatchAgent/Metrics");
+
         logger.setNamespace("Canary");
         logger.setDimensions(
                 DimensionSet.of(
