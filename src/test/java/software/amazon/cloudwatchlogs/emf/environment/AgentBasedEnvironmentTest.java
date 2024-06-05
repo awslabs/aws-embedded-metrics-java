d2efe2c8409eaaddcbc47bd05e032c2d7de88feb
Paul Schellenberg <paulsche@amazon.com>
2024-06-05 13:54:24 -0700

    replace double space indents with 4 space indents

--- src/test/java/software/amazon/cloudwatchlogs/emf/environment/AgentBasedEnvironmentTest.java
+++ src/test/java/software/amazon/cloudwatchlogs/emf/environment/AgentBasedEnvironmentTest.java
@@ -20,72 +20,72 @@ import static org.powermock.api.mockito.PowerMockito.mock;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest( {SystemWrapper.class, AgentBasedEnvironment.class} )
 public class AgentBasedEnvironmentTest {
-  public static class AgentBasedEnvironmentTestImplementation extends AgentBasedEnvironment {
-    protected AgentBasedEnvironmentTestImplementation(Configuration config) { super(config); }
-    @Override
-    public boolean probe() { return false; }
-    @Override
-    public String getType() { return null; }
-    @Override
-    public void configureContext(MetricsContext context) { }
-  }
+    public static class AgentBasedEnvironmentTestImplementation extends AgentBasedEnvironment {
+        protected AgentBasedEnvironmentTestImplementation(Configuration config) { super(config); }
+        @Override
+        public boolean probe() { return false; }
+        @Override
+        public String getType() { return null; }
+        @Override
+        public void configureContext(MetricsContext context) { }
+    }
 
-  private Configuration configuration;
+    private Configuration configuration;
 
-  @Before
-  public void setup() {
-    this.configuration = new Configuration();
-  }
+    @Before
+    public void setup() {
+        this.configuration = new Configuration();
+    }
 
-  @Test
-  public void testGetSinkWithDefaultEndpoint() throws Exception {
-    AgentSink mockedSink = mock(AgentSink.class);
-    PowerMockito.whenNew(AgentSink.class).withAnyArguments().then(invocation -> {
-      Endpoint endpoint = invocation.getArgument(2);
-      assertEquals(Endpoint.DEFAULT_TCP_ENDPOINT, endpoint);
-      return mockedSink;
-    });
+    @Test
+    public void testGetSinkWithDefaultEndpoint() throws Exception {
+        AgentSink mockedSink = mock(AgentSink.class);
+        PowerMockito.whenNew(AgentSink.class).withAnyArguments().then(invocation -> {
+            Endpoint endpoint = invocation.getArgument(2);
+            assertEquals(Endpoint.DEFAULT_TCP_ENDPOINT, endpoint);
+            return mockedSink;
+        });
 
-    AgentBasedEnvironment env = new AgentBasedEnvironmentTestImplementation(configuration);
-    ISink sink = env.getSink();
+        AgentBasedEnvironment env = new AgentBasedEnvironmentTestImplementation(configuration);
+        ISink sink = env.getSink();
 
-    assertEquals(mockedSink, sink);
-  }
+        assertEquals(mockedSink, sink);
+    }
 
-  @Test
-  public void testGetSinkWithConfiguredEndpoint() throws Exception {
-    String endpointUrl = "http://configured-endpoint:1234";
-    configuration.setAgentEndpoint(endpointUrl);
-    AgentSink mockedSink = mock(AgentSink.class);
-    PowerMockito.whenNew(AgentSink.class).withAnyArguments().then(invocation -> {
-      Endpoint endpoint = invocation.getArgument(2);
-      assertEquals(Endpoint.fromURL(endpointUrl), endpoint);
-      return mockedSink;
-    });
+    @Test
+    public void testGetSinkWithConfiguredEndpoint() throws Exception {
+        String endpointUrl = "http://configured-endpoint:1234";
+        configuration.setAgentEndpoint(endpointUrl);
+        AgentSink mockedSink = mock(AgentSink.class);
+        PowerMockito.whenNew(AgentSink.class).withAnyArguments().then(invocation -> {
+            Endpoint endpoint = invocation.getArgument(2);
+            assertEquals(Endpoint.fromURL(endpointUrl), endpoint);
+            return mockedSink;
+        });
 
-    AgentBasedEnvironment env = new AgentBasedEnvironmentTestImplementation(configuration);
-    ISink sink = env.getSink();
+        AgentBasedEnvironment env = new AgentBasedEnvironmentTestImplementation(configuration);
+        ISink sink = env.getSink();
 
-    assertEquals(mockedSink, sink);
-  }
+        assertEquals(mockedSink, sink);
+    }
 
-  @Test
-  public void testGetSinkOverrideToStdOut() {
-    PowerMockito.mockStatic(SystemWrapper.class);
-    PowerMockito.when(SystemWrapper.getenv("WRITE_TO_STDOUT")).thenReturn("true");
+    @Test
+    public void testGetSinkOverrideToStdOut() {
+        PowerMockito.mockStatic(SystemWrapper.class);
+        PowerMockito.when(SystemWrapper.getenv("WRITE_TO_STDOUT")).thenReturn("true");
 
-    AgentBasedEnvironment env = new AgentBasedEnvironmentTestImplementation(configuration);
-    ISink sink = env.getSink();
+        AgentBasedEnvironment env = new AgentBasedEnvironmentTestImplementation(configuration);
+        ISink sink = env.getSink();
 
-    assertEquals(ConsoleSink.class, sink.getClass());
-  }
+        assertEquals(ConsoleSink.class, sink.getClass());
+    }
 
-  @Test
-  public void testGetSinkOverrideToStdOutFailFastOnImproperOverride() throws Exception {
-    PowerMockito.mockStatic(SystemWrapper.class);
-    // Will only override if this env is explicitly "true"
-    PowerMockito.when(SystemWrapper.getenv("WRITE_TO_STDOUT")).thenReturn("notABool");
+    @Test
+    public void testGetSinkOverrideToStdOutFailFastOnImproperOverride() throws Exception {
+        PowerMockito.mockStatic(SystemWrapper.class);
+        // Will only override if this env is explicitly "true"
+        PowerMockito.when(SystemWrapper.getenv("WRITE_TO_STDOUT")).thenReturn("notABool");
 
-    testGetSinkWithDefaultEndpoint();
-  }
+        testGetSinkWithDefaultEndpoint();
+    }
 }
