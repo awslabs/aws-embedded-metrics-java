ea61fafe647762aca11ba604a86fcf79fb2f1c6c
Boxuan Hu <boxuan@amazon.com>
2025-01-29 22:00:57 +0000

    Replace User creds with Role creds for CWL agent

--- bin/start-agent.sh
+++ bin/start-agent.sh
@@ -18,8 +18,25 @@ tempfile="$rootdir/src/integration-test/resources/agent/.temp"
 # Configure and start the agent
 ###################################
 
+# Store the AWS STS assume-role output and extract credentials directly
+CREDS=$(aws sts assume-role \
+    --role-arn arn:aws:iam::863722843142:role/CodeBuildExecutionRole \
+    --role-session-name "session-$(uuidgen)" \
+    --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
+    --output text \
+    --duration-seconds 3600) 
+
+# Parse the output into separate variables
+read AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< $CREDS
+
+# Export the variables
+export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+
 pushd $rootdir/src/integration-test/resources/agent
 echo "[AmazonCloudWatchAgent]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+aws_session_token = $AWS_SESSION_TOKEN
 " > ./.aws/credentials
 
 echo "[profile AmazonCloudWatchAgent]
@@ -29,5 +46,8 @@ region = $AWS_REGION
 docker build -t agent:latest .
 docker run  -p 25888:25888/udp -p 25888:25888/tcp  \
     -e AWS_REGION=$AWS_REGION \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     agent:latest &> $tempfile &
 popd
